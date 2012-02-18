a4759c45cff4a43ac3fa5a2fa5e45ea2412ec502
brainster <brainster-one@hotmail.com>
2012-02-18 14:47:35 +0700

    Global reorganization

--- Sources/Khrussk.Peers/Events/IPeerEventHandler.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace Khrussk.Peers.Events {
-	/// <summary>Peer event handler.</summary>
-	public interface IPeerEventHandler {
-		/// <summary>Handle event.</summary>
-		/// <param name="e">Event args.</param>
-		void Handle(PeerEventArgs e);
-	}
-}
--- Sources/Khrussk/Properties/AssemblyInfo.cs
+++ Sources/Khrussk/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Khrussk.Peers")]
+[assembly: AssemblyTitle("Khrussk")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Khrussk.Peers")]
+[assembly: AssemblyProduct("Khrussk")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ab2c560d-2a0d-4eaa-a619-3fb30bac0077")]
+[assembly: Guid("467df988-69b8-4087-804b-4ae4c2cc0430")]
 
 // Version information for an assembly consists of the following four values:
 //
