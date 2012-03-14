f127b3108344b2e1bba1545d8c357a491fcbb699
brainster <brainster-one@hotmail.com>
2012-03-14 23:02:23 +0700

    Khrussk.NetworkRealm.Silverlight package.

--- Packages/build-packages.cmd
+++ Packages/build-packages.cmd
@@ -1,5 +1,6 @@
 ..\Assets\NuGet.exe pack Khrussk.Interfaces.nuspec
 ..\Assets\NuGet.exe pack Khrussk.Peers.nuspec
 ..\Assets\NuGet.exe pack Khrussk.NetworkRealm.nuspec
+..\Assets\NuGet.exe pack Khrussk.NetworkRealm.Silverlight.nuspec
 ..\Assets\NuGet.exe pack Khrussk.Extras.nuspec
 pause
