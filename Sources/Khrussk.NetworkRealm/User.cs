e3704e20eb8480dafa53f2b0a0e1309a90f65f3e
brainster-one <brainster-one@hotmail.com>
2012-03-18 15:49:50 +0700

    User can store related objects.

--- Sources/Khrussk.NetworkRealm/User.cs
+++ Sources/Khrussk.NetworkRealm/User.cs
@@ -1,12 +1,21 @@
 ﻿
 namespace Khrussk.NetworkRealm {
 	using System;
+	using System.Collections.Generic;
 
 	/// <summary>User.</summary>
 	public class User {
 		/// <summary>Initializes a new instance of the User class using the specified session.</summary>
 		public User(Guid session) {
 			Session = session;
+		}
+
+		/// <summary>Gets or sets related to user objects.</summary>
+		/// <param name="index">Index.</param>
+		/// <returns>Object.</returns>
+		public object this[string index] {
+			get { return _objects[index]; }
+			set { _objects[index] = value; }
 		}
 
 		/// <summary>Returns string representation of object.</summary>
@@ -17,5 +26,8 @@ namespace Khrussk.NetworkRealm {
 
 		/// <summary>Gets session Id.</summary>
 		public Guid Session { get; private set; }
+
+		/// <summary>Related to user objects.</summary>
+		private Dictionary<string, object> _objects = new Dictionary<string, object>();
 	}
 }
