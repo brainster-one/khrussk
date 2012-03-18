8d029336ae5d856255f33cc9a18e3812f02aeeb5
brainster-one <brainster-one@hotmail.com>
2012-03-18 13:55:28 +0700

    Issue #10

--- Sources/Khrussk.NetworkRealm/RealmClientEventArgs.cs
+++ Sources/Khrussk.NetworkRealm/RealmClientEventArgs.cs
@@ -1,7 +1,6 @@
 ﻿
 namespace Khrussk.NetworkRealm {
 	using System;
-	using Khrussk.Peers;
 	using Khrussk.NetworkRealm.Protocol;
 
 	public class EntityInfo {
@@ -11,17 +10,7 @@ namespace Khrussk.NetworkRealm {
 	}
 
 	public class RealmClientEventArgs : EventArgs {
-
 		public Guid Session { get; set; }
 		public EntityInfo EntityInfo { get; set; }
-
-	}
-
-	public class RealmServiceEventArgs : EventArgs {
-		/// <summary>Gets or sets user.</summary>
-		public User User { get; set; }
-
-		/// <summary>Gets or sets packet.</summary>
-		public object Packet { get; set; }
 	}
 }
---
+++ Sources/Khrussk.NetworkRealm/RealmServiceEventArgs.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Khrussk.NetworkRealm {
+	using System;
+
+	public class RealmServiceEventArgs : EventArgs {
+		/// <summary>Gets or sets user.</summary>
+		public User User { get; set; }
+
+		/// <summary>Gets or sets packet.</summary>
+		public object Packet { get; set; }
+	}
+}
