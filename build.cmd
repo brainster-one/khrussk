97381c2677116ac2c158963f8c0c5b5d232aeb14
brainster-one <brainster-one@hotmail.com>
2012-04-22 15:53:24 +0700

    Packages has been merged.

--- build.cmd
+++ build.cmd
@@ -1,9 +1,4 @@
-Assets\MSBuild.exe .\Sources\Khrussk.Interfaces.proj
-Assets\MSBuild.exe .\Sources\Khrussk.Peers.proj
-Assets\MSBuild.exe .\Sources\Khrussk.NetworkRealm.proj
-Assets\MSBuild.exe .\Sources\Khrussk.NetworkRealm.Silverlight.proj 
+Assets\MSBuild.exe .\Sources\Khrussk.proj
+Assets\MSBuild.exe .\Sources\Khrussk.Silverlight.proj
 
-Assets\NuGet.exe pack .\Packages\Specifications\Khrussk.Interfaces.nuspec -OutputDirectory packages
-Assets\NuGet.exe pack .\Packages\Specifications\Khrussk.Peers.nuspec -OutputDirectory packages
-Assets\NuGet.exe pack .\Packages\Specifications\Khrussk.NetworkRealm.nuspec -OutputDirectory packages
-Assets\NuGet.exe pack .\Packages\Specifications\Khrussk.NetworkRealm.Silverlight.nuspec -OutputDirectory packages
+Assets\NuGet.exe pack .\Packages\Specifications\Khrussk.nuspec -OutputDirectory packages
